b9306c1f1aa1e97beb600d0e88a26ff46eaee37e
Your Name <ianpilon@gmail.com>
2025-03-31 11:47:25 -0400

    Initial Commit

---
+++ MacAudioRecorder/MacAudioRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacAudioRecorderApp.swift
+//  MacAudioRecorder
+//
+//  Created by Ian Pilon on 2025-03-31.
+//
+
+import SwiftUI
+
+@main
+struct MacAudioRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
